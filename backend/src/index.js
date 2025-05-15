00e16e8e8ed01100881501a52ee20975f4998cfe
Shivam <sk3002882@gmail.com>
2025-05-15 15:18:37 +0530

    / removed

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,8 +20,8 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: process.env.NODE_ENV === "production" 
-      ? [process.env.FRONTEND_URL, "https://mernstack-chatapp.onrender.com"] 
+    origin: process.env.NODE_ENV === "production"
+      ? [process.env.FRONTEND_URL, "https://mernstack-chatapp.onrender.com"]
       : "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
@@ -48,7 +48,7 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/*", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
